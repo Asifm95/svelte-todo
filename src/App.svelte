4f6333b2cb53b3dcd5d5b1ff78c3ab19ddfd4541
Asif Mohammed <asif.m@fayausa.com>
2019-08-08 23:46:26 +0530

    inital commit :tada:

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name;
+</script>
+
+<style>
+
+</style>
+
+<div class="flex justify-center">
+  <div class="bg-gray-400">hello world</div>
+  <div class="bg-gray-600 visible">how are you</div>
+  <div class="bg-gray-400">i am svelete!!</div>
+</div>
