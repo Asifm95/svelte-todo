037331d33d9d661c4cbb2b78082d4ac3e6aacb3a
Asifm95 <asifm2882@gmail.com>
2019-08-09 12:10:08 +0530

    add cehckbox + list template :art:

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
 <div class="container mx-auto py-10 flex flex-col items-center">
   <img class="w-56 object-center" src={'/img/app-logo.svg'} alt="" />
   <div class="w-1/2 my-5">
-    <form class="w-full">
+    <form class="w-full focus-within:border-teal-500">
       <div class="flex items-center border-b border-b-2 py-2">
         <input
           class="appearance-none bg-transparent border-none w-full text-gray-700
@@ -18,13 +18,38 @@
           placeholder="Jane Doe"
           aria-label="Full name" />
         <button
-          class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500
-          hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          class="flex-shrink-0 bg-purple hover:bg-purple border-purple
+          hover:border-pruple text-sm border-4 text-white py-1 px-2 rounded"
           type="button">
           Add
         </button>
       </div>
     </form>
+  </div>
+  <div class="w-1/2 flex flex-col items-center">
+    <ul class="list">
+      <li class="list-item">
+        <input type="checkbox" class="hidden-box" id="first" checked />
+        <label for="first" class="check--label">
+          <span class="check--label-box" />
+          <span class="check--label-text">First Checkbox</span>
+        </label>
+      </li>
+      <li class="list-item">
+        <input type="checkbox" class="hidden-box" id="second" />
+        <label for="second" class="check--label">
+          <span class="check--label-box" />
+          <span class="check--label-text">Second Checkbox</span>
+        </label>
+      </li>
+      <li class="list-item">
+        <input type="checkbox" class="hidden-box" id="third" />
+        <label for="third" class="check--label">
+          <span class="check--label-box" />
+          <span class="check--label-text">Third Checkbox</span>
+        </label>
+      </li>
+    </ul>
   </div>
 
 </div>
